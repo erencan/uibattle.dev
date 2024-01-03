4068dcd16f7d6573f35279213774e6765cee9735
erencan <erencan.ec@gmail.com>
2024-01-04 02:18:47 +0300

    Proje bilgileri güncellendi.

---
+++ README.md
@@ -0,0 +1,109 @@
+# UIbattle.dev
+
+### Overview:
+UIBattle is a platform designed for interface developers to enhance their skills and engage in friendly competitions with other developers. The platform encourages continuous improvement by regularly releasing daily interface challenges for developers to tackle. Through these challenges, developers aim to replicate the provided user interfaces as closely as possible in their code.
+
+### Daily Interface Challenges:
+
+UIBattle regularly publishes daily interface challenges, each presenting unique design aspects and coding intricacies.
+Developers participating in these challenges strive to reproduce the given user interfaces with precision, testing their skills and creativity.
+
+### Competitive Environment:
+
+The platform fosters a competitive environment where developers can measure their proficiency against others.
+Success is quantified through accuracy in replicating interfaces, providing developers with a clear understanding of their strengths and areas for improvement.
+
+### Performance Metrics:
+
+Developers can view success percentages, allowing them to assess their performance objectively.
+These metrics serve as a benchmark for improvement and a source of motivation for developers to enhance their coding and design skills.
+
+### Social Integration:
+
+UIBattle offers seamless integration with social media platforms, enabling developers to share their achievements, challenges, and success percentages.
+This feature promotes a sense of community and encourages collaboration among developers.
+
+### Benefits:
+
+#### Skill Enhancement:
+
+Developers have the opportunity to enhance their UI development skills by consistently engaging in challenging tasks.
+Competition and Motivation:
+
+The competitive aspect of UIBattle motivates developers to continually improve their abilities.
+
+#### Community Building:
+
+The platform facilitates the formation of a supportive community where developers can connect, share insights, and learn from each other.
+
+## Setup
+
+Make sure to install the dependencies:
+
+```bash
+# npm
+npm install
+
+# pnpm
+pnpm install
+
+# yarn
+yarn install
+
+# bun
+bun install
+```
+
+## Development Server
+
+Start the development server on `http://localhost:3000`:
+
+```bash
+# npm
+npm run dev
+
+# pnpm
+pnpm run dev
+
+# yarn
+yarn dev
+
+# bun
+bun run dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+# npm
+npm run build
+
+# pnpm
+pnpm run build
+
+# yarn
+yarn build
+
+# bun
+bun run build
+```
+
+Locally preview production build:
+
+```bash
+# npm
+npm run preview
+
+# pnpm
+pnpm run preview
+
+# yarn
+yarn preview
+
+# bun
+bun run preview
+```
+
+Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
--- uibattle-client/app.vue
+++ uibattle-client/app.vue
@@ -1,5 +1,139 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+    <div class="container">
+        <div class="main">
+            <div class="header">
+                <img src="/img/logo.svg">
+                <div style="width: 100%; padding: 20px; text-align: right">Challenge Day #124</div>
+            </div>
+            <div class="target-ui">
+                <div class="target-ui__title">Target UI</div>
+                <div class="target-ui__image">
+                    <table>
+                        <tr>
+                            <td class="target-ui__top-left"></td>
+                            <td></td>
+                            <td class="target-ui__top-right"></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><img src="/img/target-ui.png"></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td class="target-ui__bottom-left"></td>
+                            <td></td>
+                            <td class="target-ui__bottom-right"></td>
+                        </tr>
+                        <tr>
+                            <td colspan="3" style="text-align: center">550x400 px</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="target-ui__join">
+                    <button class="target-ui__join_button">Join Challenge</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Kodchasan:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+body, html, #__nuxt {
+  margin: 0;
+  padding: 0;
+    font-family: 'Kodchasan', sans-serif;
+    height: 100%;
+}
+
+.container {
+  background-color: #f9f9f9;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.main {
+  min-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.header {
+  width: 100%;
+  display: flex;
+  margin-bottom: 20px;
+}
+
+.target-ui {
+  background-color: white;
+  border-radius: 16px;
+  padding: 30px 20px;
+
+  &__title {
+      padding-left: 20px;
+    font-size: 24px;
+    font-weight: bold;
+    margin-bottom: 20px;
+  }
+
+  &__top-left {
+    width: 10px;
+    height: 10px;
+    border-right: grey 1px solid;
+    border-bottom: gray 1px solid;
+  }
+
+  &__top-right {
+    width: 10px;
+    height: 10px;
+    border-left: grey 1px solid;
+    border-bottom: gray 1px solid;
+  }
+
+  &__bottom-left {
+    width: 10px;
+    height: 10px;
+    border-right: grey 1px solid;
+    border-top: gray 1px solid;
+
+  }
+
+  &__bottom-right {
+    width: 10px;
+    height: 10px;
+    border-left: grey 1px solid;
+    border-top: gray 1px solid;
+  }
+&__join {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    &_button {
+        font-size: 16px;
+        color: white;
+        padding: 10px 20px;
+        border-radius: 16px;
+        background-color: #696969;
+        cursor: pointer;
+        outline: none;
+        transition: all 0.2s ease-in-out;
+        &:hover {
+            opacity: 0.8;
+        }
+        &:active {
+            opacity: 0.5;
+        }
+    }
+}
+
+}
+</style>
+<script setup lang="ts">
+useHead({
+    title: 'UIbattle.dev'
+})
+</script>
